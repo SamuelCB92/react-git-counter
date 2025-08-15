e97dae5d8f4dca8ae1696f9e01f4e6cd98c26fa7
Samuel <sam.makes.programs@gmail.com>
2025-08-15 02:17:05 -0300

    add basic counter component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+      <h1>React Counter for Git</h1>
+      <div className="count-display">{count}</div>
+      <div className="button-group">
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
